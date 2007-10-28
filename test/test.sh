e7bc4fe15b454773c041f50121aac278e7b99aac
Brendan MacDonell <macdonellba@gmail.com>
2007-10-28 12:46:39 -0300

    New and expanded tests in test.sh.

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,12 @@
 # They'll hardly need to be comprehensive anyway, given such a small codebase.
 
 NTX=../src/ntx
+TAB="	"
 export NTXROOT=`pwd`/"ntx_test"
+
+if [ -d $NTXROOT ]; then
+  rm -r $NTXROOT
+fi
 
 # Used to manipulate ntx add/edit.
 function _ntx {
@@ -15,7 +20,7 @@ function _ntx {
 }
 
 function die {
-  rm -r $NTXROOT
+#  rm -r $NTXROOT
   exit -1
 }
 
@@ -30,16 +35,30 @@ function assert {
 
 # Set up the notes through a series of clever hacks.
 V=`_ntx a.sh add soulfu git todo`
+Av="Create a community soulfu git repository for the *nix p..."
 Ai=`echo $V | cut -b 1-4`
-assert "`echo $V | cut -b 6-`" "Create a community soulfu git repository for the *nix p..."
+assert "`echo $V | cut -b 6-`" "$Av"
 
 V=`_ntx b.sh add ntx todo`
+Bv="Add deletion support to ntx via a tagged/ directory."
 Bi=`echo $V | cut -b 1-4`
-assert "`echo $V | cut -b 6-`" "Add deletion support to ntx via a tagged/ directory."
+assert "`echo $V | cut -b 6-`" "$Bv"
 
 V=`_ntx c.sh add pacman COW todo`
+Cv="Complete the Copy-On-Write pacman pseudo-FS/database."
 Ci=`echo $V | cut -b 1-4`
-assert "`echo $V | cut -b 6-`" "Complete the Copy-On-Write pacman pseudo-FS/database."
+assert "`echo $V | cut -b 6-`" "$Cv"
+
+# Test listing notes - FIFO ordering is assumed.
+assert "`$NTX list`" "$Ai$TAB$Av
+$Bi$TAB$Bv
+$Ci$TAB$Cv"
+assert "`$NTX list todo`" "$Ai$TAB$Av
+$Bi$TAB$Bv
+$Ci$TAB$Cv"
+assert "`$NTX list todo git`" "$Ai$TAB$Av"
+assert "`$NTX list pacman ntx`" \
+"No notes exist in the intersection of those tags."
 
 # Test listing tags. Unfortunately, can't guarantee the order.
 TAGS=`$NTX tag`
@@ -61,10 +80,30 @@ COW
 todo"
 
 # Test per-tag retagging. Again, we test for a defined set and order.
-# Re-test above tag tests.
+$NTX tag $Ai soulfu git todo unix
+assert "`$NTX tag $Ai`" "soulfu
+git
+todo
+unix"
+$NTX tag $Ci pacman todo COW unix
+assert "`$NTX tag $Ci`" "pacman
+todo
+COW
+unix"
 
-# Test a full-index listing.
-# Test several combinations and single tags.
+# Re-test above tag tests.
+TAGS=`$NTX tag`
+assert "$TAGS" "*soulfu*"
+assert "$TAGS" "*git*"
+assert "$TAGS" "*todo*"
+assert "$TAGS" "*ntx*"
+assert "$TAGS" "*pacman*"
+assert "$TAGS" "*COW*"
+assert "$TAGS" "*unix*"
+
+# Re-test listing notes for unix.
+assert "`$NTX list unix`" "$Ai$TAB$Av
+$Ci$TAB$Cv"
 
 # Test altering a note.
 # Re-test above list tests.
